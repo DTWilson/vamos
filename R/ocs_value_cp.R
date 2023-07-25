9b491f06040422eee549a0bddde27547c19667f8
Duncan Wilson <meddwilb@leeds.ac.uk>
2023-07-25 15:53:55 +0100

    Added search_points() to compute points on hypothesis boundaries

--- R/ocs_value_cp.R
+++ R/ocs_value_cp.R
@@ -10,18 +10,7 @@ ocs_value_cp <- function(n, null, alternative, cor_z, alpha_nom, variance, a, c_
 
   tI <- sqrt(value_cp_obj(crit, cor_z, alpha_nom, a, c_x, c_y, n_y, b_null)) + alpha_nom
 
-  w <- (c_x)/(c_y - n_y - a*c_x*n_y)
-  s_a <- (c_x - b_alt)/c_x
-
-  z_1 <- (seq(sqrt(0.001), sqrt(10), length.out = 50))^2 + b_alt
-  z_2 <- c_y - s_a*(c_y - (w*c_y - (c_x - (c_x - z_1)/s_a))/(w + w*a*(c_x - (c_x - z_1)/s_a)))
-
-  df <- data.frame(z_1 = z_1, z_2 = z_2)
-  df <- df[df$z_1 <= c_x & df$z_2 <= c_y & df$z_2 >= n_y,]
-
-  df <- rbind(data.frame(z_1 = rep(b_alt, 50), z_2 = seq(15, c_y, length.out = 50)),
-              df,
-              data.frame(z_1 = seq(c_x, 15, length.out = 50), z_2 = rep(df[nrow(df), "z_2"], 50)))
+  df <- search_points(a, c_x, c_y, n_y, b_alt)
 
   # Run a a simple exhaustive search
   tII <- NULL
@@ -50,18 +39,7 @@ value_cp_obj <- function(crit, cor_z, alpha_nom, a, c_x, c_y, n_y, b_null) {
 
   # Search over the null hypothesis boundary to find the maximum type I error
   # rate.
-  w <- (c_x)/(c_y - n_y - a*c_x*n_y)
-  s_n <- (c_x - b_null)/c_x
-
-  z_1 <- (seq(sqrt(0.001), sqrt(10), length.out = 50))^2 + b_null
-  z_2 <- c_y - s_n*(c_y - (w*c_y - (c_x - (c_x - z_1)/s_n))/(w + w*a*(c_x - (c_x - z_1)/s_n)))
-
-  df <- data.frame(z_1 = z_1, z_2 = z_2)
-  df <- df[df$z_1 <= c_x & df$z_2 <= c_y & df$z_2 >= n_y,]
-
-  df <- rbind(data.frame(z_1 = rep(b_null, 50), z_2 = seq(15, c_y, length.out = 50)),
-              df,
-              data.frame(z_1 = seq(c_x, 15, length.out = 50), z_2 = rep(df[nrow(df), "z_2"], 50)))
+  df <- search_points(a, c_x, c_y, n_y, b_null)
 
   # Run a a simple exhaustive search
   tI <- NULL
@@ -97,7 +75,6 @@ int_cond_z_1_cp <- function(z_1, mean, cor_z, a, c_x, c_y, crit) {
 
   # Calculate the conditional prob and weight by the marginal
   # prob of z_1
-  #print(c(z_1, (1 - pnorm(low, m, s))*dnorm(z_1, mean[1], 1)))
   (1 - pnorm(low, m, s))*dnorm(z_1, mean[1], 1)
 }
 
@@ -116,18 +93,7 @@ ocs_value_cp_wrong <- function(n, null, alternative, cor_z, alpha_nom, variance,
   crit <- opt$par
   tI <- sqrt(opt$value) + alpha_nom
 
-  w <- (c_x)/(c_y - n_y - a*c_x*n_y)
-  s_a <- (c_x - b_alt)/c_x
-
-  z_1 <- (seq(sqrt(0.001), sqrt(10), length.out = 50))^2 + b_alt
-  z_2 <- c_y - s_a*(c_y - (w*c_y - (c_x - (c_x - z_1)/s_a))/(w + w*a*(c_x - (c_x - z_1)/s_a)))
-
-  df <- data.frame(z_1 = z_1, z_2 = z_2)
-  df <- df[df$z_1 <= c_x & df$z_2 <= c_y & df$z_2 >= n_y,]
-
-  df <- rbind(data.frame(z_1 = rep(b_alt, 50), z_2 = seq(15, c_y, length.out = 50)),
-              df,
-              data.frame(z_1 = seq(c_x, 15, length.out = 50), z_2 = rep(df[nrow(df), "z_2"], 50)))
+  df <- search_points(a, c_x, c_y, n_y, b_alt)
 
   # Run a a simple exhaustive search
   tII <- NULL
@@ -144,18 +110,7 @@ value_cp_wrong_obj <- function(crit, cor_z, alpha_nom, a, c_x, c_y, n_y, b_null)
 
   # Search over the null hypothesis boundary to find the maximum type I error
   # rate.
-  w <- (c_x)/(c_y - n_y - a*c_x*n_y)
-  s_n <- (c_x - b_null)/c_x
-
-  z_1 <- (seq(sqrt(0.001), sqrt(10), length.out = 50))^2 + b_null
-  z_2 <- c_y - s_n*(c_y - (w*c_y - (c_x - (c_x - z_1)/s_n))/(w + w*a*(c_x - (c_x - z_1)/s_n)))
-
-  df <- data.frame(z_1 = z_1, z_2 = z_2)
-  df <- df[df$z_1 <= c_x & df$z_2 <= c_y & df$z_2 >= n_y,]
-
-  df <- rbind(data.frame(z_1 = rep(b_null, 50), z_2 = seq(15, c_y, length.out = 50)),
-              df,
-              data.frame(z_1 = seq(c_x, 15, length.out = 50), z_2 = rep(df[nrow(df), "z_2"], 50)))
+  df <- search_points(a, c_x, c_y, n_y, b_null)
 
   # Run a a simple exhaustive search
   tI <- NULL
@@ -167,4 +122,23 @@ value_cp_wrong_obj <- function(crit, cor_z, alpha_nom, a, c_x, c_y, n_y, b_null)
 
   # Return the penalised objective to be minimised
   return((max(tI)  - alpha_nom)^2)
+}
+
+
+search_points <- function(a, c_x, c_y, n_y, b, size = 50) {
+  # Build a data frame of points along a hypothesis boundary
+  w <- (c_x)/(c_y - n_y - a*c_x*n_y)
+  s <- (c_x - b)/c_x
+
+  z_1 <- (seq(sqrt(0.001), sqrt(10), length.out = size))^2 + b
+  z_2 <- c_y - s*(c_y - (w*c_y - (c_x - (c_x - z_1)/s))/(w + w*a*(c_x - (c_x - z_1)/s)))
+
+  df <- data.frame(z_1 = z_1, z_2 = z_2)
+  df <- df[df$z_1 <= c_x & df$z_2 <= c_y & df$z_2 >= n_y,]
+
+  df <- rbind(data.frame(z_1 = rep(b, size), z_2 = seq(15, c_y, length.out = size)),
+              df,
+              data.frame(z_1 = seq(c_x, 15, length.out = size), z_2 = rep(df[nrow(df), "z_2"], size)))
+
+  return(df)
 }
